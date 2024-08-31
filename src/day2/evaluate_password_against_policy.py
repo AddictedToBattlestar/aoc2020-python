f8f17d06e129023f94e2c89e6f1787824c57e6bc
AddictedToBattlestar <addictedtobsg@gmail.com>
2024-08-31 15:39:22 -0400

    Day 1 part 2

--- src/day2/evaluate_password_against_policy.py
+++ src/day2/evaluate_password_against_policy.py
@@ -1,28 +1,50 @@
 from src.utilities.file_reader import read_lines_from_file
+from enum import Enum, auto
 
+class PolicyTypeEnum(Enum):
+    TOBOGGAN = auto()
+    SLED_RENTAL = auto()
 
 def parse_policy(policy):
     policy_range, character = policy.split(" ")
     min_count, max_count = map(int, policy_range.split("-"))
     return min_count, max_count, character
 
-def evaluate(policy_and_password_line):
+def evaluate_sled_rental(policy_and_password_line):
     policy, password_line = [x.strip() for x in policy_and_password_line.split(":")]
     min_count, max_count, character = parse_policy(policy)
     character_count = password_line.count(character)
     return min_count <= character_count <= max_count
 
-def get_count_of_valid_passwords(lines):
+def evaluate_toboggan(policy_and_password_line):
+    policy, password_line = [x.strip() for x in policy_and_password_line.split(":")]
+    first_position, second_position, character_to_match = parse_policy(policy)
+    first_position_matches = password_line[first_position - 1] == character_to_match
+    second_position_matches = password_line[second_position - 1] == character_to_match
+    if first_position_matches and second_position_matches:
+        return False
+    else:
+        return first_position_matches or second_position_matches
+
+def evaluate(policy_and_password_line, policy_type):
+    if policy_type == PolicyTypeEnum.TOBOGGAN:
+        return evaluate_toboggan(policy_and_password_line)
+    elif policy_type == PolicyTypeEnum.SLED_RENTAL:
+        return evaluate_sled_rental(policy_and_password_line)
+
+def get_count_of_valid_passwords(lines, policy_type):
     valid_count = 0
     for line in lines:
-        if evaluate(line):
+        if evaluate(line, policy_type):
             valid_count += 1
     return valid_count
 
-def calculate_from_file(file_name):
+def calculate_from_file(file_name, policy_type):
     lines = read_lines_from_file(file_name)
-    return get_count_of_valid_passwords(lines)
+    return get_count_of_valid_passwords(lines, policy_type)
 
 if __name__ == '__main__':
-    part_1_result = calculate_from_file("data.txt")
+    part_1_result = calculate_from_file("data.txt", PolicyTypeEnum.SLED_RENTAL)
     print(f'The solution for Day 1, part 1 is: {part_1_result}')
+    part_2_result = calculate_from_file("data.txt", PolicyTypeEnum.TOBOGGAN)
+    print(f'The solution for Day 1, part 2 is: {part_2_result}')
--- tests/day2/test_evaluate_password_against_policy.py
+++ tests/day2/test_evaluate_password_against_policy.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
 from src.day2.evaluate_password_against_policy import parse_policy, evaluate, get_count_of_valid_passwords, \
-    calculate_from_file
+    calculate_from_file, evaluate_sled_rental, PolicyTypeEnum, evaluate_toboggan
 from src.settings import PROJECT_ROOT
-
 
 class TestEvaluatePassword(TestCase):
     def test_parse_policy(self):
@@ -10,19 +9,42 @@ class TestEvaluatePassword(TestCase):
         self.assertEqual((1,3,"b"), parse_policy("1-3 b"))
         self.assertEqual((2,9,"c"), parse_policy("2-9 c"))
 
-    def test_evaluate(self):
-        self.assertTrue(evaluate("1-3 a: abcde"))
-        self.assertFalse(evaluate("1-3 b: cdefg"))
-        self.assertTrue(evaluate("2-9 c: ccccccccc"))
+    def test_sled_rental_evaluate(self):
+        self.assertTrue(evaluate_sled_rental("1-3 a: abcde"))
+        self.assertFalse(evaluate_sled_rental("1-3 b: cdefg"))
+        self.assertTrue(evaluate_sled_rental("2-9 c: ccccccccc"))
 
-    def test_get_count_of_valid_passwords(self):
-        self.assertEqual(2, get_count_of_valid_passwords([
-            "1-3 a: abcde",
-            "1-3 b: cdefg",
-            "2-9 c: ccccccccc",
-        ]))
+    def test_sled_rental_get_count_of_valid_passwords(self):
+        self.assertEqual(2, get_count_of_valid_passwords(
+            lines=[
+                "1-3 a: abcde",
+                "1-3 b: cdefg",
+                "2-9 c: ccccccccc"
+            ],
+            policy_type=PolicyTypeEnum.SLED_RENTAL
+        ))
 
-    def test_get_count_of_valid_passwords_from_sample_file(self):
+    def test_sled_rental_get_count_of_valid_passwords_from_sample_file(self):
         sample_data_test_file = PROJECT_ROOT + "/day2/sample-data.txt"
-        actual_result = calculate_from_file(sample_data_test_file)
+        actual_result = calculate_from_file(sample_data_test_file, PolicyTypeEnum.SLED_RENTAL)
         self.assertEqual(actual_result, 2)
+
+    def test_toboggan_evaluate(self):
+        self.assertTrue(evaluate_toboggan("1-3 a: abcde"))
+        self.assertFalse(evaluate_toboggan("1-3 b: cdefg"))
+        self.assertFalse(evaluate_toboggan("2-9 c: ccccccccc"))
+
+    def test_toboggan_get_count_of_valid_passwords(self):
+        self.assertEqual(1, get_count_of_valid_passwords(
+            lines=[
+                "1-3 a: abcde",
+                "1-3 b: cdefg",
+                "2-9 c: ccccccccc"
+            ],
+            policy_type=PolicyTypeEnum.TOBOGGAN
+        ))
+
+    def test_toboggan_get_count_of_valid_passwords_from_sample_file(self):
+        sample_data_test_file = PROJECT_ROOT + "/day2/sample-data.txt"
+        actual_result = calculate_from_file(sample_data_test_file, PolicyTypeEnum.TOBOGGAN)
+        self.assertEqual(actual_result, 1)
